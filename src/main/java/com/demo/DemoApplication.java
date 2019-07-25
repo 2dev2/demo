23303b58228e83d5fc52f1afb614f9699eaf54a7
dev <dharmadev2@gmail.com>
2019-07-25 17:37:55 +0530

    spring boot is running server was taken in lib depedency-- chnaged the value from  spring-boot-starter to spring-boot-starter-web in gradle

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
 @SpringBootApplication
 @EnableScheduling
 public class DemoApplication  {
@@ -15,8 +21,22 @@ public class DemoApplication  {
 //        return application.sources(DemoApplication.class);
 //    }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName+"hi");
+            }
+
+        };
     }
 }
